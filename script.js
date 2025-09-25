7c7e53ffac5904f4b7e255a12926345613a8de4c
Jcjimenezglez <jcjimenezglez@gmail.com>
2025-09-25 02:19:30 -0400

    Replace X close button with Lucide X SVG icon

--- script.js
+++ script.js
@@ -1357,7 +1357,12 @@ class PomodoroTimer {
         // Create modal content
         const modalContent = `
             <div class="technique-info-modal">
-                <button class="close-technique-info-x">×</button>
+                <button class="close-technique-info-x">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x">
+                        <path d="M18 6 6 18"/>
+                        <path d="m6 6 12 12"/>
+                    </svg>
+                </button>
                 <h3>${info.title}</h3>
                 <p class="technique-description">${info.description}</p>
                 <div class="technique-video">
