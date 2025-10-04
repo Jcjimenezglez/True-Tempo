6e3028c25f4c79cfa85d1cc75a128d6c6ae2f810
Jcjimenezglez <jcjimenezglez@gmail.com>
2025-10-04 10:48:44 -0400

    style: remove color from guest streak modal icon
    - Use currentColor instead of #ff6b35 for consistency
    - Icon now matches upgrade modal styling (gray color)
    - Maintains visual consistency across all modals

--- script.js
+++ script.js
@@ -5621,7 +5621,7 @@ class PomodoroTimer {
             </button>
             <div class="upgrade-content">
                 <div class="upgrade-icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#ff6b35" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M12 3q1 4 4 6.5t3 5.5a1 1 0 0 1-14 0 5 5 0 0 1 1-3 1 1 0 0 0 5 0c0-2-1.5-3-1.5-5q0-2 2.5-4"/>
                     </svg>
                 </div>
