7e61c372c6862885d9730be3d57d6b7c57d23787
Jcjimenezglez <jcjimenezglez@gmail.com>
2026-01-20 16:42:36 -0500

    Update timer creation limit modal text
    - Change to: 'manage your focus effectively'
    - More professional and clear message

--- script.js
+++ script.js
@@ -1505,7 +1505,7 @@ class PomodoroTimer {
                     reason: 'lifetime_limit_reached'
                 });
                 
-                this.showCustomTechniqueProModal('You\'ve used your free timer creation. Upgrade to Premium to create unlimited custom timers and control your focus.', lifetimeCreated);
+                this.showCustomTechniqueProModal('You\'ve used your free timer creation. Upgrade to Premium to create unlimited custom timers and manage your focus effectively.', lifetimeCreated);
             } else {
                 // Free user can create their first timer
                 const form = document.getElementById('customForm');
